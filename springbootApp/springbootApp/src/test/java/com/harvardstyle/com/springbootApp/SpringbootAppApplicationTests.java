b16ce39cd8f7764d0bfeee75b888c2ec8f3d17ad
JinendraBogahawatte <82801300+JinendraBogahawatte@users.noreply.github.com>
2022-05-21 00:26:07 -0700

    Add files via upload

---
+++ springbootApp/springbootApp/src/test/java/com/harvardstyle/com/springbootApp/SpringbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harvardstyle.com.springbootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
